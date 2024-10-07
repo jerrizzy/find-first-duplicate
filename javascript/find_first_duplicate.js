96916859718fefc7a480e6c5fbdd670b1b1fdeac
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-10-06 22:58:55 -0400

    solution

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,16 @@
 function findFirstDuplicate(arr) {
   // type your code here
-  
+  const mySet = new Set()
+  result = -1
+
+  for ( i = 0; i < arr.length; i++) {
+    if (mySet.has(arr[i])) {
+      return arr[i]
+    } else {
+      mySet.add(arr[i])
+    }
+  }
+  return result 
 }
 
 if (require.main === module) {
@@ -19,15 +29,20 @@ module.exports = findFirstDuplicate;
 // Please add your pseudocode to this file
 /*******************************************************************
  * initialize a variable and set it equal to -1
+ * initialize an empty set. Set holds only unique elements. No duplicate
  * 
  * iterate through an array:
- *  if a duplicate element is found:
- *    add it to the initialized variable
- * and return the new variable
- * otherwise return the original variable
+ *  Check if each element is in the set, if it's not:
+ *    add it to the set
+ *    if the element is already in the set:
+ *  return the element
+ * otherwise return the variable equal to -1
  * 
- */ 
-// And a written explanation of your solution
-// iterate through an array
-// and if a duplicate element is found
-// return it, and if none found return -1
+ */
+// I use a Set to store all the iterated values.
+// A Set is a data structure that holds unique element or values
+// I iterate through the array
+// check if the value is in the set, if not, add it.
+// if the value is already in the set return that value
+// otherwise if we go through the array and no duplicate is found
+// return -1
